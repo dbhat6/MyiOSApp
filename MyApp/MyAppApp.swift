e56d53e41473cdcb7ae3a04a849bf917e00e7315
Deepak Bhat <deepakbht67@gmail.com>
2023-07-27 13:58:10 +0100

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Deepak Bhat on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
