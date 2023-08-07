4912e7c1ce8579f8d918be6c636e1eb2899e0a33
Deepak Bhat <deepakbht67@gmail.com>
2023-08-07 01:40:41 +0100

    added first commit

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -6,18 +6,121 @@
 //
 
 import SwiftUI
+//import Alamofire
 
 struct ContentView: View {
+    @State private var showingForm = false
+    let myGrayColor : Color = Color(red: 0.651, green: 0.651, blue: 0.651)
+    let myBlackColor : Color = Color(red: 0, green: 0, blue: 0)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            NavigationStack {
+                ZStack {
+                    Image("wave")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .opacity(1.0)
+                
+                    
+
+                
+                    VStack {
+                        Spacer()
+                        Text("MyApp")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(myBlackColor.opacity(0.4))
+                            .cornerRadius(15)
+                        
+                        Spacer()
+                        
+                        NavigationLink {
+                            ExpiryTrackerView()
+                        } label: {
+                            Text("Expiry Tracker")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(myBlackColor.opacity(0.4))
+                                .cornerRadius(15)
+                        }
+                    
+                        NavigationLink {
+                            FormView()
+                        } label: {
+                            Text("Grocery Inventory")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(myBlackColor.opacity(0.4))
+                                .cornerRadius(15)
+                        }
+                        Spacer()
+                        Spacer()
+                    }.padding()
+                }
+            }
         }
-        .padding()
     }
 }
+
+struct FormView: View {
+    @State private var name: String = ""
+    @State private var email: String = ""
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.5)
+            VStack {
+                Text("Grocery Inventory")
+                    .foregroundColor(Color.white)
+
+                Form {
+                    TextField("Name", text: $name)
+                    TextField("Email", text: $email)
+                    Button(action: {
+                        // Action to perform when Submit button is tapped
+                        print("Submit button tapped")
+                        //                sendDataToGoogleSheets()
+                    }) {
+                        Text("Submit")
+                    }
+                }
+                .background(Color.clear)
+            }
+        }
+    }
+}
+
+//func sendDataToGoogleSheets(name: String, email: String) {
+//    // API endpoint URL
+//    let url = "https://sheets.googleapis.com/v4/spreadsheets/YOUR_SPREADSHEET_ID/values/Sheet1?key=YOUR_API_KEY"
+//
+//    // Form data
+//    let parameters: [String: Any] = [
+//        "range": "A2:B2", // The cell range where you want to write the data
+//        "majorDimension": "ROWS",
+//        "values": [
+//            [name, email]
+//        ]
+//    ]
+//
+//    // Make the POST request
+//    AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+//        switch response.result {
+//        case .success:
+//            print("Data sent to Google Sheets successfully!")
+//        case .failure(let error):
+//            print("Error sending data to Google Sheets: \(error)")
+//        }
+//    }
+//}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ MyApp/ExpiryTracker.swift
@@ -0,0 +1,120 @@
+//
+//  ExpiryTracker.swift
+//  MyApp
+//
+//  Created by Deepak Bhat on 06/08/23.
+//
+
+import Foundation
+import SwiftUI
+import UserNotifications
+
+struct ExpiryTrackerView: View {
+    @State private var items = [ExpiryItem]()
+    @State private var showingForm = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(items) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text("Expiry Date: \(item.expiryDate, formatter: itemFormatter)")
+                            .foregroundColor(item.isExpired ? .red : .secondary)
+                    }
+                }
+                
+                Button("Add Item") {
+//                    addItem()
+                    self.showingForm.toggle()
+                }
+                if showingForm {
+                    AddExpiryView(onSubmit: addItem)
+                }
+            }
+            .navigationBarTitle("Expiry Tracker")
+        }
+    }
+    
+    func addItem(name: String, expiryDate: Date) {
+        let newItem = ExpiryItem(name: name, expiryDate: expiryDate) // Replace this with the actual expiry date of the item
+        items.append(newItem)
+        
+        // Schedule a notification for the expiry date
+        scheduleNotification(for: newItem)
+    }
+    
+    
+    
+    
+    private func scheduleNotification(for item: ExpiryItem) {
+        let content = UNMutableNotificationContent()
+        content.title = "Item Expiry Alert"
+        content.body = "\(item.name) has expired!"
+        content.sound = UNNotificationSound.default
+        
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: item.expiryDate)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+    }
+}
+
+private struct AddExpiryView: View {
+    @State private var name: String = ""
+    @State private var date: Date = Date()
+    let onSubmit: (String, Date) -> Void
+    let structInstance = ExpiryTrackerView()
+
+    var body: some View {
+        Form {
+            TextField("Name", text: $name)
+            DatePicker(
+                    "Expiry Date",
+                    selection: $date,
+                    displayedComponents: [.date, .hourAndMinute]
+                )
+
+            Button(action: {
+                // Action to perform when Submit button is tapped
+                print("Submit button tapped")
+                print("Name: \(name)")
+                print("Date: \(date)")
+                onSubmit(name, date)
+                //                sendDataToGoogleSheets()
+            }) {
+                Text("Submit")
+            }
+        }
+    }
+}
+
+
+
+struct ExpiryTrackerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpiryTrackerView()
+    }
+}
+
+
+
+struct ExpiryItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let expiryDate: Date
+    
+    var isExpired: Bool {
+        return expiryDate < Date()
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
